dc11c78df333732edd6e6ed4112ec265844b47d8
Damian2057 <ddamian2057@gmail.com>
2023-03-09 20:37:37 +0100

    NumberOfSentencesExtractor implementation

--- TextDocumentClassification/src/main/java/p/lodz/pl/logic/extractor/types/AverageWordLengthExtractor.java
+++ TextDocumentClassification/src/main/java/p/lodz/pl/logic/extractor/types/AverageWordLengthExtractor.java
@@ -7,10 +7,12 @@ import p.lodz.pl.model.Feature;
 
 import java.util.List;
 
+import static p.lodz.pl.constants.Const.COMMON_WORDS_DICTIONARY;
+
 public class AverageWordLengthExtractor extends Extractor implements SpecificExtractor {
     @Override
     public Feature<?> extract(Article article) {
-        List<String> words = CommonWords.removeCommonWords(article);
+        List<String> words = CommonWords.removeByDictionary(article, COMMON_WORDS_DICTIONARY);
         double avg = 0;
         for (String word : words) {
             avg += word.length();
--- TextDocumentClassification/src/main/java/p/lodz/pl/logic/extractor/types/NumberOfSentencesExtractor.java
+++ TextDocumentClassification/src/main/java/p/lodz/pl/logic/extractor/types/NumberOfSentencesExtractor.java
@@ -1,12 +1,25 @@
 package p.lodz.pl.logic.extractor.types;
 
+import p.lodz.pl.enums.Type;
 import p.lodz.pl.model.Article;
 import p.lodz.pl.model.Feature;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class NumberOfSentencesExtractor implements SpecificExtractor {
 
     @Override
     public Feature<Double> extract(Article article) {
-        return null;
+        String text = article.getBody();
+
+        Pattern pattern = Pattern.compile("\\.\\s");
+        Matcher matcher = pattern.matcher(text);
+        double count = 0;
+        while (matcher.find()) {
+            count++;
+        }
+
+        return new Feature<Double>(Type.NUMBER_OF_SENTENCES, count);
     }
 }
