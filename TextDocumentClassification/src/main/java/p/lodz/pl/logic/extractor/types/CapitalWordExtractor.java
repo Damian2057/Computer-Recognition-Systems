59a0bdf4fadc2a25f82ea223edd80221006cb08e
Damian2057 <ddamian2057@gmail.com>
2023-03-10 13:18:52 +0100

    CapitalWordExtractor implementation

--- TextDocumentClassification/src/main/java/p/lodz/pl/logic/extractor/types/CapitalWordExtractor.java
+++ TextDocumentClassification/src/main/java/p/lodz/pl/logic/extractor/types/CapitalWordExtractor.java
@@ -1,12 +1,22 @@
 package p.lodz.pl.logic.extractor.types;
 
+import p.lodz.pl.enums.Type;
 import p.lodz.pl.logic.extractor.types.base.SpecificExtractor;
 import p.lodz.pl.model.Article;
 import p.lodz.pl.model.Feature;
 
+import java.util.Arrays;
+
 public class CapitalWordExtractor implements SpecificExtractor {
     @Override
-    public Feature<?> extract(Article article) {
-        return null;
+    public Feature<Double> extract(Article article) {
+        String text = article.getBody();
+        double count = Arrays.stream(text.split("\\.\\s+"))
+                .flatMap(sentence -> Arrays.stream(sentence.split("\\s+")))
+                .skip(1)
+                .filter(word -> Character.isUpperCase(word.charAt(0)))
+                .count();
+
+        return new Feature<>(Type.CAPITAL_WORD, count);
     }
 }
