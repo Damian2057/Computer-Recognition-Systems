9d0220816adec17c8fb234e5dd6a35b2a42caf6a
Damian2057 <ddamian2057@gmail.com>
2023-03-10 13:24:25 +0100

    CountryExtractor KeyWordExtractor and PlacesExtractor implementation

--- TextDocumentClassification/src/main/java/p/lodz/pl/logic/extractor/types/KeyWordExtractor.java
+++ TextDocumentClassification/src/main/java/p/lodz/pl/logic/extractor/types/KeyWordExtractor.java
@@ -1,14 +1,18 @@
 package p.lodz.pl.logic.extractor.types;
 
+import p.lodz.pl.enums.Type;
 import p.lodz.pl.logic.extractor.types.base.DictionaryExtractor;
 import p.lodz.pl.logic.extractor.types.base.SpecificExtractor;
 import p.lodz.pl.model.Article;
 import p.lodz.pl.model.Feature;
 
+import static p.lodz.pl.constants.Const.KEY_WORDS_DICTIONARY;
+
 public class KeyWordExtractor extends DictionaryExtractor implements SpecificExtractor {
 
     @Override
-    public Feature<?> extract(Article article) {
-        return null;
+    public Feature<String> extract(Article article) {
+        return new Feature<>(Type.KEY_WORD,
+                mostCommonItem(article, KEY_WORDS_DICTIONARY).orElse("undefined"));
     }
 }
