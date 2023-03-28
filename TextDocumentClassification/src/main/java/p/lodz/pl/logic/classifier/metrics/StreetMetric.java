fbc5f5bb507956578f918c1646f746debf746e2a
Damian2057 <ddamian2057@gmail.com>
2023-03-28 17:38:13 +0200

    implementation of ChebyshevMetric and Street metrics

--- TextDocumentClassification/src/main/java/p/lodz/pl/logic/classifier/metrics/StreetMetric.java
+++ TextDocumentClassification/src/main/java/p/lodz/pl/logic/classifier/metrics/StreetMetric.java
@@ -6,6 +6,16 @@ import p.lodz.pl.model.Vector;
 public class StreetMetric extends SpecificMetric implements Metric {
     @Override
     public Neighbor calculateMetric(Vector vector, Vector neighbor) {
-        return null;
+        double distance = 0.0;
+        for (int i = 0; i < vector.getFeatures().size(); i++) {
+            if (vector.getFeatures().get(i).getFeature() instanceof Double) {
+                distance += Math.abs(vector.getFeatures().get(i).getAsDouble() -
+                        neighbor.getFeatures().get(i).getAsDouble());
+            } else {
+                distance += Math.abs(measure.getDistanceBetweenTwoWords(vector.getFeatures().get(i).getAsString(),
+                        neighbor.getFeatures().get(i).getAsString()));
+            }
+        }
+        return new Neighbor(neighbor.getArticleRealCountry(), distance);
     }
 }
