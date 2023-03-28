36d826b1f49ed0d33a91bdad1bedc680b060bba5
Damian2057 <ddamian2057@gmail.com>
2023-03-28 15:01:59 +0200

    interface method

--- TextDocumentClassification/src/main/java/p/lodz/pl/logic/classifier/measures/Measure.java
+++ TextDocumentClassification/src/main/java/p/lodz/pl/logic/classifier/measures/Measure.java
@@ -1,4 +1,5 @@
 package p.lodz.pl.logic.classifier.measures;
 
 public interface Measure {
+    double getDistanceBetweenTwoWords(String word1, String word2);
 }
--- TextDocumentClassification/src/main/java/p/lodz/pl/logic/classifier/measures/NGramMeasure.java
+++ TextDocumentClassification/src/main/java/p/lodz/pl/logic/classifier/measures/NGramMeasure.java
@@ -1,4 +1,8 @@
 package p.lodz.pl.logic.classifier.measures;
 
 public class NGramMeasure implements Measure {
+    @Override
+    public double getDistanceBetweenTwoWords(String word1, String word2) {
+        return 0;
+    }
 }
