ab5d3ff9360601da86a31b6e49d547a11a000245
Damian2057 <ddamian2057@gmail.com>
2023-05-05 18:16:43 +0200

    init

---
+++ LinguisticSummariesOfDatabases/src/test/java/p/lodz/pl/LinguisticSummariesOfDatabasesApplicationTests.java
@@ -0,0 +1,13 @@
+package p.lodz.pl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinguisticSummariesOfDatabasesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
