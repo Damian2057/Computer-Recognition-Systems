89104c7f2220bfdffab1f4100a1b43d935386036
aniaxb <236493@edu.p.lodz.pl>
2023-05-25 12:06:51 +0200

    Configured GUI elements

--- LinguisticSummaries/src/main/java/p/lodz/pl/frontend/components/AdvancedController.java
+++ LinguisticSummaries/src/main/java/p/lodz/pl/frontend/components/AdvancedController.java
@@ -1,4 +1,31 @@
 package p.lodz.pl.frontend.components;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
+import javafx.scene.layout.AnchorPane;
+
 public class AdvancedController {
+    public Button goBackButton;
+    public AnchorPane summarizerLabelsPane;
+    public TitledPane quantifierLabelsPane;
+    public AnchorPane quantifierLabels;
+    public AnchorPane addAnchorPane;
+    public ChoiceBox addAttributeTypeChoiceBox;
+    public TextField addEtiquetteNameTextField;
+    public ChoiceBox addAttributeNameChoiceBox;
+    public ChoiceBox addFunctionTypeChoiceBox;
+    public Button addButton;
+    public AnchorPane editAnchorPane;
+    public ChoiceBox editAttributeTypeChoiceBox;
+    public ChoiceBox editAttributeNameChoiceBox;
+    public ChoiceBox editFunctionTypeChoiceBox;
+    public Button editButton;
+    public ChoiceBox editEtiquetteNameChoiceBox;
+    public AnchorPane removeAnchorPane;
+    public ChoiceBox removeAttributeTypeChoiceBox;
+    public ChoiceBox removeAttributeNameChoiceBox;
+    public ChoiceBox removeEtiquetteNameChoiceBox;
+    public Button removeButton;
 }
--- LinguisticSummaries/src/main/java/p/lodz/pl/frontend/components/WeightedQMController.java
+++ LinguisticSummaries/src/main/java/p/lodz/pl/frontend/components/WeightedQMController.java
@@ -1,4 +1,17 @@
 package p.lodz.pl.frontend.components;
 
+import javafx.scene.control.TextField;
+
 public class WeightedQMController {
+    public TextField degreeOfTruthTF;
+    public TextField degreeOfImprecisionTF;
+    public TextField degreeOfCoveringTF;
+    public TextField degreeOfAppropriatenessTF;
+    public TextField lengthOfSummaryTF;
+    public TextField degreeOfQuantifierImprecisionTF;
+    public TextField degreeOfQuantifierRelativeCardinalityTF;
+    public TextField degreeOfSummarizerRelativeCardinalityTF;
+    public TextField degreeOfQualifierImprecisionTF;
+    public TextField degreeOfQualifierRelativeCardinalityTF;
+    public TextField lengthOfQualifierTF;
 }
