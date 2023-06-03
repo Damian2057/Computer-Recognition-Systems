17d33a7a15a1430245fd26801c2aaeb00c69cf5c
Damian2057 <ddamian2057@gmail.com>
2023-06-03 13:22:53 +0200

    fix for CustomCellFactory

--- LinguisticSummaries/src/main/java/p/lodz/pl/frontend/components/CustomCellFactory.java
+++ LinguisticSummaries/src/main/java/p/lodz/pl/frontend/components/CustomCellFactory.java
@@ -1,15 +1,15 @@
 package p.lodz.pl.frontend.components;
 
-import javafx.beans.value.ObservableValue;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.util.Callback;
 
-public class CustomCellFactory<Summary> implements Callback<TableColumn<Summary, Double>, TableCell<Summary, Double>> {
+public class CustomCellFactory<T> implements Callback<TableColumn<T, Double>,
+        TableCell<T, Double>> {
 
     @Override
-    public TableCell<Summary, Double> call(TableColumn<Summary, Double> column) {
-        return new TableCell<Summary, Double>() {
+    public TableCell<T, Double> call(TableColumn<T, Double> column) {
+        return new TableCell<>() {
             @Override
             protected void updateItem(Double item, boolean empty) {
                 super.updateItem(item, empty);
@@ -21,22 +21,14 @@ public class CustomCellFactory<Summary> implements Callback<TableColumn<Summary,
                         formattedValue = "1.00";
                     } else if (item > 0.99) {
                         formattedValue = ">" + "0.99";
-                    } else if (item < 0.01){
+                    } else if (item == 0.0) {
+                        formattedValue = "0.00";
+                    } else if (item < 0.01) {
                         formattedValue = "~" + "0.00";
                     } else formattedValue = String.format("%.2f", item);
                     setText(formattedValue);
                 }
             }
         };
-    }
-
-    private String formatValue(Double value) {
-        if (value == 0.9) {
-            return "~1.00";
-        } else if (value > 0.99) {
-            return ">" + String.format("%.2f", value);
-        } else {
-            return "~" + String.format("%.2f", value);
-        }
     }
 }
